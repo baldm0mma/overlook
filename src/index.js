571712047b4d6da120dcf62e45973d883ec905db
Jev Forsberg <jev.forsberg@gmail.com>
2019-05-29 16:45:07 -0600

    Fetch complete and working

--- src/index.js
+++ src/index.js
@@ -1,13 +1,51 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you import jQuery into a JS file if you use jQuery in that file
-import $ from 'jquery';
-
-// An example of how you tell webpack to use a CSS (SCSS) file
+// import $ from 'jquery';
 import './css/base.scss';
+import fetch from 'cross-fetch';
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+let userData;
+let roomData;
+let booksingsData;
+let roomServicesData;
 
-console.log('This is the JavaScript entry file - your code begins here.');
+fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1903/users/users')
+  .then(response => response.json())
+  .then(data => {
+    userData = data.users;
+});
+
+fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1903/rooms/rooms')
+  .then(response => response.json())
+  .then(data => {
+    roomData = data.rooms;
+});
+
+fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1903/bookings/bookings')
+  .then(response => response.json())
+  .then(data => {
+    booksingsData = data.bookings;
+});
+
+fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1903/room-services/roomServices')
+  .then(response => response.json())
+  .then(data => {
+    roomServicesData = data.roomServices;
+});
+
+// let urls = ['https://fe-apps.herokuapp.com/api/v1/overlook/1903/users/users', 'https://fe-apps.herokuapp.com/api/v1/overlook/1903/rooms/rooms', 'https://fe-apps.herokuapp.com/api/v1/overlook/1903/bookings/bookings', 'https://fe-apps.herokuapp.com/api/v1/overlook/1903/room-services/roomServices'];
+
+// let variables = [userData, roomData, booksingsData, roomServicesData];
+
+// Promise.all(urls.map(url=>fetch(url))).then(responses =>
+//   Promise.all(responses.map(res => res.json()))
+// ).then(data.map => {
+//   variables.map(eachVar => data = eachVar);
+// }) 
+
+function timer() {
+  console.log(userData);
+  console.log(roomData);
+  console.log(booksingsData);
+  console.log(roomServicesData);
+}
+
+setTimeout(timer, 1000);
