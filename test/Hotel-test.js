d4d6af66949f88135b9e666d9263e60fd4e9bac6
Jev Forsberg <jev.forsberg@gmail.com>
2019-05-30 15:51:26 -0600

    Add createNewCustomer and captureReturnCustomer methods to Hotel class

--- test/Hotel-test.js
+++ test/Hotel-test.js
@@ -2,34 +2,33 @@ import chai from 'chai';
 const expect = chai.expect;
 import spies from 'chai-spies';
 chai.use(spies);
-import AllCustomers from '../src/AllCustomers';
+import Hotel from '../src/Hotel';
 import customerTestData from '../src/testData/TestData';
 // import domUpdates from "../src/domUpdates";
 // chai.spy.on(domUpdates, 'updateScore', () => true);
 // chai.spy.on(domUpdates, 'turnPrompt', () => true);
 
-describe('AllCustomers', function() {
+describe('Hotel', function() {
 
-  let allCustomers;
+  let hotel;
 
   beforeEach(function() {
-    allCustomers = new AllCustomers(customerTestData);
+    hotel = new Hotel();
 
   });
 
   it('should be a function', function() {
-    expect(AllCustomers).to.be.a('function');
+    expect(Hotel).to.be.a('function');
   });
 
-  it('should be an instance of AllCustomers', function() {
-    expect(allCustomers).to.be.an.instanceof(AllCustomers);
+  it('should be an instance of Hotel', function() {
+    expect(hotel).to.be.an.instanceof(Hotel);
   });
 
   it('should have a method that returns a user object when given a search parameter of a name, or ID, and, should return "null" if the user does not exsist', function() {
-    // console.log(AllCustomersTestData);
-    expect(allCustomers.searchForSpecificUser('Autumn')).to.be.an('object');
-    expect(allCustomers.searchForSpecificUser('1')).to.be.an('object');
-    expect(allCustomers.searchForSpecificUser('Jev')).to.equal(null);
+    expect(hotel.searchForSpecificUser('Autumn')).to.be.an('object');
+    expect(hotel.searchForSpecificUser('1')).to.be.an('object');
+    expect(hotel.searchForSpecificUser('Jev')).to.equal(null);
   });
 
 });
