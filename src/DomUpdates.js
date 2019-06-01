30263af274b624c0c6c55c9efe53141734735085
Jev Forsberg <jev.forsberg@gmail.com>
2019-06-01 10:47:55 -0600

    Add basic customer tab styling

--- src/DomUpdates.js
+++ src/DomUpdates.js
@@ -1,23 +1,35 @@
 import $ from 'jquery';
 // import Admin from './Admin';
 
+let currentCustomer;
+
 const domUpdates = {
+
+  updateTimeAndDateAtInterval(admin) {
+    $('#todays-date').text(admin.today).hide().fadeIn(2000);
+    // $('#current-time').text(setInterval(new Date().toLocaleTimeString(), 1000));
+  },
   
   populateAllTabs(admin) {
-    $('.total-rooms').text(admin.hotel.generateAvailableRoomNumbers().length);
-    $('.total-earned').text(admin.hotel.calculateDebtsToday());
-    $('.percent-occupied').text(admin.hotel.showPercentageOfRoomsOccupiedToday());
-    // domUpdates.populateCatagories(game);
+    $('.total-rooms').text(admin.hotel.generateAvailableRoomNumbers().length).hide().fadeIn(2000);
+    $('.total-earned').text(admin.hotel.calculateDebtsToday()).hide().fadeIn(2000);
+    $('.percent-occupied').text(admin.hotel.showPercentageOfRoomsOccupiedToday()).hide().fadeIn(2000);
   },
 
-  populateCatagories(game) {
-    $('.main__game-category-1').text(game.round.categoryTitles[0]);
-    $('.main__game-category-2').text(game.round.categoryTitles[1]);
-    $('.main__game-category-3').text(game.round.categoryTitles[2]);
-    $('.main__game-category-4').text(game.round.categoryTitles[3]);
-    game.round.startingPrompt();
-    domUpdates.populateClues(game);
+  returnSeacrhedCustomerName(admin, searchTerm) {
+    currentCustomer = admin.searchForSpecificUser(searchTerm);
+    if (currentCustomer) {
+      $('#returned-name').text('Were you searching for ' + currentCustomer.name + '?');
+      $('form').trigger('reset');
+    } else {
+      $('#returned-name').text('Sorry, they don\'t exsist in our database yet.');
+    }
   },
+
+  makeSearchedCustomerFocus() {
+    $('#current-customer-info').text('We\'re currently focusing on ' + currentCustomer.name + ', customer ID: ' + currentCustomer.id);
+    $('#returned-name').text('Great!').fadeOut(5000);
+  }
 
 };
 
